e3afc893697271daabc5ebf3c01883f74cc7765b
Barry Chen <barry.chen@udnshopping.com>
2021-03-11 09:58:46 +0800

    Initial Commit

---
+++ MyMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMind
+//
+//  Created by Barry Chen on 2021/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
