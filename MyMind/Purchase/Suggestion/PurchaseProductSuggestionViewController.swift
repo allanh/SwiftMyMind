1588d32a555fb256ecb76175ee14961813ce832a
Barry Chen <fm334142@gmail.com>
2021-06-02 11:35:50 +0800

    feat: connect IBOutlet for `PurchaseProductSuggestionViewController`

--- MyMind/Purchase/Suggestion/PurchaseProductSuggestionViewController.swift
+++ MyMind/Purchase/Suggestion/PurchaseProductSuggestionViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class PurchaseProductSuggestionViewController: UIViewController {
 
+    @IBOutlet private weak var productImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var originalNumberLabel: UILabel!
+    @IBOutlet private weak var suggestedQuantityButton: UIButton!
+    @IBOutlet private weak var suggestedQuantityLabel: UILabel!
+    @IBOutlet private weak var purchaseCostTextField: UITextField!
+    @IBOutlet private weak var purchaseQuantityTextField: UITextField!
+    @IBOutlet private weak var unitLabel: UILabel!
+    @IBOutlet private weak var boxCounterLabel: UILabel!
+    @IBOutlet private weak var totalPurchaseCostTextField: UITextField!
+    @IBOutlet private weak var deleteButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
