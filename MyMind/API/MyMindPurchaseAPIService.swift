74c6fc0dd635284ee70c158012a27dc456998d5f
Barry Chen <barry.chen@udnshopping.com>
2021-06-08 18:05:36 +0800

    chore: move `Warehouse` to it's own file

--- MyMind/API/MyMindPurchaseAPIService.swift
+++ MyMind/API/MyMindPurchaseAPIService.swift
@@ -66,27 +66,3 @@ class MyMindPurchaseAPIService: PromiseKitAPIService {
 }
 
 extension MyMindPurchaseAPIService: PurchaseAPIService { }
-
-struct Warehouse: Codable {
-    struct RecipientInfo: Codable {
-        let name, phone: String
-        let address: Address
-    }
-
-    struct Address: Codable {
-        let county, district, zipcode, address: String
-    }
-
-    let name, type, id, number: String
-    let channelWareroomID: String
-    let recipientInfo: RecipientInfo
-
-    enum CodingKeys: String, CodingKey {
-        case name
-        case type = "warehouse_type"
-        case id = "warehouse_id"
-        case number = "warehouse_no"
-        case channelWareroomID = "channel_wareroom_id"
-        case recipientInfo = "recipient_info"
-    }
-}
---
+++ MyMind/Model/Warehouse.swift
@@ -0,0 +1,33 @@
+//
+//  Warehouse.swift
+//  MyMind
+//
+//  Created by Barry Chen on 2021/6/8.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import Foundation
+
+struct Warehouse: Codable {
+    struct RecipientInfo: Codable {
+        let name, phone: String
+        let address: Address
+    }
+
+    struct Address: Codable {
+        let county, district, zipcode, address: String
+    }
+
+    let name, type, id, number: String
+    let channelWareroomID: String
+    let recipientInfo: RecipientInfo
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case type = "warehouse_type"
+        case id = "warehouse_id"
+        case number = "warehouse_no"
+        case channelWareroomID = "channel_wareroom_id"
+        case recipientInfo = "recipient_info"
+    }
+}
