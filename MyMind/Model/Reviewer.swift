fa93918dea0c57073d566338b0b7183a2226747f
Barry Chen <barry.chen@udnshopping.com>
2021-06-08 18:26:30 +0800

    feat: implement `Reviewer` model

---
+++ MyMind/Model/Reviewer.swift
@@ -0,0 +1,19 @@
+//
+//  Reviewer.swift
+//  MyMind
+//
+//  Created by Barry Chen on 2021/6/8.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import Foundation
+
+struct Reviewer: Codable {
+    let id: String
+    let account: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "employee_id"
+        case account
+    }
+}
