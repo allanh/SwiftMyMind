1799842ee3f818df408f0de09a0a00df38c34e55
Barry Chen <barry.chen@udnshopping.com>
2021-06-09 18:21:09 +0800

    chore: move `ApplyPurchaseParameterInfo` to it's own file

--- MyMind/Purchase/Apply/PurchaseInfoViewModel.swift
+++ MyMind/Purchase/Apply/PurchaseInfoViewModel.swift
@@ -137,37 +137,3 @@ struct PickReviewerViewModel {
             }
     }
 }
-
-struct ApplyPurchaseParameterInfo: Codable {
-    // MARK: - ProductInfo
-    struct ProductInfo: Codable {
-        let productID, purchaseQuantity, proposedQuantity, channelStockQuantity: String
-        let fineStockQuantity, purchaseCost, totalPrice: String
-
-        enum CodingKeys: String, CodingKey {
-            case productID = "product_id"
-            case purchaseQuantity = "purchase_quantity"
-            case proposedQuantity = "proposed_quantity"
-            case channelStockQuantity = "channel_stock_quantity"
-            case fineStockQuantity = "fine_stock_quantity"
-            case purchaseCost = "purchase_cost"
-            case totalPrice = "total_price"
-        }
-    }
-
-    let partnerID, vendorID, expectStorageDate, reviewBy: String
-    let remark, expectWarehouseID: String
-    let expectWarehouseType: Warehouse.WarehouseType
-    let productInfo: [ProductInfo]
-
-    enum CodingKeys: String, CodingKey {
-        case partnerID = "partner_id"
-        case vendorID = "vendor_id"
-        case expectStorageDate = "expect_storage_date"
-        case reviewBy = "review_by"
-        case remark
-        case expectWarehouseType = "expect_warehouse_type"
-        case expectWarehouseID = "expect_warehouse_id"
-        case productInfo = "product_info"
-    }
-}
