9c0cfcec8366e5e5c0ff3f041f5978b8a9cdc709
Barry Chen <fm334142@gmail.com>
2021-06-22 16:56:52 +0800

    feat: implement `PurchaseLogInfoViewController`

---
+++ MyMind/Purchase/CompletedApply/View/PurchaseLogInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PurchaseLogInfoViewController.swift
+//  MyMind
+//
+//  Created by Chen Yi-Wei on 2021/6/22.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import UIKit
+
+final class PurchaseLogInfoViewController: UIViewController {
+
+    @IBOutlet private weak var stackView: UIStackView!
+
+    var logInfos: [PurchaseOrder.LogInfo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureContentWithLogInfos()
+    }
+
+    private func makeLogInfoView(with logInfo: PurchaseOrder.LogInfo) -> LogInfoView {
+        let logInfoView = LogInfoView()
+        logInfoView.configure(with: logInfo)
+        return logInfoView
+    }
+
+    private func configureContentWithLogInfos() {
+        logInfos.forEach {
+            let logInfoView = makeLogInfoView(with: $0)
+            stackView.addArrangedSubview(logInfoView)
+        }
+    }
+}
