8ef44c8837567035bc9c32f643dfaba0eae34390
Barry Chen <fm334142@gmail.com>
2021-06-22 18:23:21 +0800

    feat: implement `PurchaseCompletedApplyViewController`

---
+++ MyMind/Purchase/CompletedApply/View/PurchaseCompletedApplyViewController.swift
@@ -0,0 +1,192 @@
+//
+//  PurchaseCompletedApplyViewController.swift
+//  MyMind
+//
+//  Created by Chen Yi-Wei on 2021/6/22.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import UIKit
+
+final class PurchaseCompletedApplyViewController: NiblessViewController {
+    // MARK: - Properties
+    private var contentViewControllers: [UIViewController] = []
+
+    let purchaseID: String
+    let loader: PurchaseOrderLoader
+    var purchaseOrder: PurchaseOrder?
+
+    // MARK: UI
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionHeadersPinToVisibleBounds = true
+        let screenWidth = UIScreen.main.bounds.width
+        layout.headerReferenceSize = CGSize(width: screenWidth, height: 120)
+        let horizontalInset: CGFloat = 20
+        layout.sectionInset = UIEdgeInsets(top: 15, left: horizontalInset, bottom: 15, right: horizontalInset)
+        let collecitonView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collecitonView.backgroundColor = .white
+        return collecitonView
+    }()
+
+    let backToHomeButton: UIButton = UIButton {
+        $0.backgroundColor = .white
+        $0.setTitleColor(UIColor(hex: "004477"), for: .normal)
+        $0.setTitle("儲存", for: .normal)
+        $0.titleLabel?.font = .pingFangTCSemibold(ofSize: 16)
+    }
+
+    let backToListButton: UIButton = UIButton {
+        $0.backgroundColor = UIColor(hex: "004477")
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitle("回採購管理", for: .normal)
+        $0.titleLabel?.font = .pingFangTCSemibold(ofSize: 16)
+    }
+
+    let buttonsStackView: UIStackView = UIStackView {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+    }
+
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        constructViewHierarchy()
+        activateConstraintsStackView()
+        configCollectionView()
+    }
+    // MARK: - Methods
+    init(purchaseID: String, loader: PurchaseOrderLoader) {
+        self.purchaseID = purchaseID
+        self.loader = loader
+        super.init()
+    }
+
+    private func constructViewHierarchy() {
+        view.addSubview(collectionView)
+        buttonsStackView.addArrangedSubview(backToHomeButton)
+        buttonsStackView.addArrangedSubview(backToListButton)
+        view.addSubview(buttonsStackView)
+    }
+
+    private func activateConstraints() {
+        activateConstraintsCollecitonView()
+        activateConstraintsStackView()
+    }
+
+    private func configCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.registerCell(ContainerCollectionViewCell.self)
+        collectionView.register(StageProgressCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: StageProgressCollectionReusableView.self))
+    }
+
+    private func loadPurchaseOrder() {
+        startAnimatingActivityIndicator()
+        loader.loadPurchaseOrder(with: purchaseID)
+            .ensure {
+                self.stopAnimatinActivityIndicator()
+            }
+            .done { [weak self] order in
+                guard let self = self else { return }
+                self.purchaseOrder = order
+                self.generateContentViewControllers(with: order)
+                self.collectionView.reloadData()
+            }.catch { error in
+                print(error.localizedDescription)
+                #warning("Error handling")
+            }
+    }
+
+    private func generateContentViewControllers(with purchaseOrder: PurchaseOrder) {
+        let purchaseOrderInfoViewController = PurchaseOrderInfoViewController.loadFormNib()
+        purchaseOrderInfoViewController.purchaseOrder = purchaseOrder
+        purchaseOrderInfoViewController.didTapCheckPurchasedProductButton = {
+            #warning("show product details view controller")
+        }
+        contentViewControllers.append(purchaseOrderInfoViewController)
+
+        let purchaseLogInfoViewController = PurchaseLogInfoViewController.loadFormNib()
+        purchaseLogInfoViewController.logInfos = purchaseOrder.logInfos
+        contentViewControllers.append(purchaseLogInfoViewController)
+    }
+}
+// MARK: - Collection view data source
+extension PurchaseCompletedApplyViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return contentViewControllers.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(ContainerCollectionViewCell.self, for: indexPath) as? ContainerCollectionViewCell else {
+            print("Wrong cell identifier or not register yet")
+            return UICollectionViewCell()
+        }
+        let viewController = contentViewControllers[indexPath.item]
+        cell.hostedView = viewController.view
+
+        return cell
+    }
+}
+// MARK: - Collection view delegate flow layout
+extension PurchaseCompletedApplyViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: StageProgressCollectionReusableView.self), for: indexPath)
+        view.backgroundColor = .white
+        (view as? StageProgressCollectionReusableView)?.configProgressView(numberOfStage: 3, stageTitleList: ["採購建議", "採購申請", "送出審核"], currentStageIndex: 2)
+        return view
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 125)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        var horizontalInsets: CGFloat = .zero
+        if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+            horizontalInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right
+        }
+        let width = screenWidth - horizontalInsets
+
+        switch indexPath.item {
+        case 0: return CGSize(width: width, height: 575)
+        default: return CGSize(width: width, height: 400)
+        }
+    }
+}
+// MARK: - Layouts
+extension PurchaseCompletedApplyViewController {
+    private func activateConstraintsCollecitonView() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        let top = collectionView.topAnchor
+            .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        let leading = collectionView.leadingAnchor
+            .constraint(equalTo: view.leadingAnchor)
+        let bottom = collectionView.bottomAnchor
+            .constraint(equalTo: buttonsStackView.topAnchor)
+        let trailing = collectionView.trailingAnchor
+            .constraint(equalTo: view.trailingAnchor)
+
+        NSLayoutConstraint.activate([
+            top, leading, bottom, trailing
+        ])
+    }
+
+    private func activateConstraintsStackView() {
+        buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        let leading = buttonsStackView.leadingAnchor
+            .constraint(equalTo: view.leadingAnchor)
+        let trailing = buttonsStackView.trailingAnchor
+            .constraint(equalTo: view.trailingAnchor)
+        let bottom = buttonsStackView.bottomAnchor
+            .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        let height = buttonsStackView.heightAnchor
+            .constraint(equalToConstant: 50)
+
+        NSLayoutConstraint.activate([
+            leading, trailing, bottom, height
+        ])
+    }
+}
