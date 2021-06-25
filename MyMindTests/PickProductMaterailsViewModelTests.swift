c7eb883cb97ef30dc1f8b997dc5e8485236f5b81
Barry Chen <fm334142@gmail.com>
2021-06-25 11:25:21 +0800

    test: remove redundant code and refactor some test

--- MyMindTests/PickProductMaterailsViewModelTests.swift
+++ MyMindTests/PickProductMaterailsViewModelTests.swift
@@ -12,80 +12,25 @@ import PromiseKit
 
 @testable import MyMind
 
-class MockPurchaseAPIService: PurchaseAPIService {
-    func fetchPurchaseSuggestionInfos(with productIDs: [String]) -> Promise<PurchaseSuggestionInfoList> {
-        .init(error: APIError.dataNotFoundError)
-    }
+class MockProductMaterialListLoader: ProductMaterialListLoader {
+    var promise: Promise<ProductMaterialList>?
+    let deferred = Promise<ProductMaterialList>.pending()
 
-    func fetchPurchaseWarehouseList() -> Promise<[Warehouse]> {
-        .init(error: APIError.dataNotFoundError)
-    }
-
-    func fetchProductMaterialDetail(with id: String) -> Promise<ProductMaterialDetail> {
-        .init(error: APIError.dataNotFoundError)
-    }
-
-    func fetchPurchaseSeggestionInfos(with productIDs: [String]) -> Promise<PurchaseSuggestionInfoList> {
-        .init(error: APIError.dataNotFoundError)
-    }
-
-    let userSession: UserSession
-    var shouldSuccess: Bool = true
-    let cachedFileDataHelper = CachedFileDataHelper()
-    var cachedMaterialListResult: ProductMaterialList?
-
-    init() {
-        userSession = .testUserSession
-    }
-
-    func fetchPurchaseList(purchaseListQueryInfo: PurchaseListQueryInfo?) -> Promise<PurchaseList> {
-        return Promise<PurchaseList> { seal in
-            DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
-                if self.shouldSuccess {
-                    guard let data = self.cachedFileDataHelper.cachedFileData(name: "purchase_list"),
-                          let purchaseList = try? JSONDecoder().decode(Response<PurchaseList>.self, from: data).data
-                    else {
-                        seal.reject(APIError.dataNotFoundError)
-                        return
-                    }
-                    seal.fulfill(purchaseList)
-                } else {
-                    seal.reject(APIError.dataNotFoundError)
-                }
-            }
-        }
-    }
-
-    func fetchProductMaterialList(with query: ProductMaterialQueryInfo) -> Promise<ProductMaterialList> {
-        return Promise<ProductMaterialList> { seal in
-            DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
-                if self.shouldSuccess {
-                    guard let data = self.cachedFileDataHelper.cachedFileData(name: "product_materials"),
-                          let productMaterialList = try? JSONDecoder().decode(Response<ProductMaterialList>.self, from: data).data
-                    else {
-                        seal.reject(APIError.dataNotFoundError)
-                        return
-                    }
-                    self.cachedMaterialListResult = productMaterialList
-                    seal.fulfill(productMaterialList)
-                } else {
-                    seal.reject(APIError.dataNotFoundError)
-                }
-            }
-        }
+    func loadProductMaterialList(with query: ProductMaterialQueryInfo) -> Promise<ProductMaterialList> {
+        return deferred.promise
     }
 }
 
 class PickProductMaterailsViewModelTests: XCTestCase {
 
     var sut: PickProductMaterialsViewModel!
-    var mockAPIService: MockPurchaseAPIService!
+    var mockLoader: MockProductMaterialListLoader!
     let cachedFileDataHelper = CachedFileDataHelper()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        mockAPIService = MockPurchaseAPIService()
-        sut = PickProductMaterialsViewModel(vendorInfo: .init(id: "", name: ""), purchaseAPIService: mockAPIService)
+        mockLoader = MockProductMaterialListLoader()
+        sut = PickProductMaterialsViewModel(vendorInfo: .init(id: "", name: ""), loader: mockLoader)
     }
 
     override func tearDownWithError() throws {
@@ -100,12 +45,14 @@ class PickProductMaterailsViewModelTests: XCTestCase {
             .observe(on: MainScheduler.instance)
             .skip(1)
 
+
         sut.refreshFetchProductMaterials(with: .init(vendorInfo: .init(id: "", name: "")))
+        mockLoader.deferred.resolver.fulfill(try makeMockProductMaterialList())
 
         let result = try currentProductMaterial.toBlocking(timeout: 1).first()
 
         XCTAssertFalse(try XCTUnwrap(result).isEmpty)
-        XCTAssertEqual(mockAPIService.cachedMaterialListResult?.currentPageNumber, sut.currentPageInfo?.currentPageNumber)
+        XCTAssertEqual(try makeMockProductMaterialList().currentPageNumber, sut.currentPageInfo?.currentPageNumber)
     }
 
     func test_fetchMoreMaterials_success() throws {
@@ -125,7 +72,9 @@ class PickProductMaterailsViewModelTests: XCTestCase {
             .skip(1)
 
         let beforePageNumer = try XCTUnwrap(sut.currentQueryInfo.pageNumber)
+
         sut.fetchMoreProductMaterials(with: &sut.currentQueryInfo)
+        mockLoader.deferred.resolver.fulfill(try makeMockProductMaterialList())
 
         let result = try currentProductMaterial.toBlocking(timeout: 1).first()
         XCTAssertEqual(try XCTUnwrap(result), productMaterialList.materials + productMaterialList.materials)
@@ -133,12 +82,7 @@ class PickProductMaterailsViewModelTests: XCTestCase {
     }
 
     func test_pickMaterials() throws {
-        guard let data = self.cachedFileDataHelper.cachedFileData(name: "product_materials"),
-              let productMaterialList = try? JSONDecoder().decode(Response<ProductMaterialList>.self, from: data).data
-        else {
-            XCTFail("data not found")
-            return
-        }
+        let productMaterialList = try makeMockProductMaterialList()
         sut.currentProductMaterials.accept(productMaterialList.materials)
         sut.selectMaterial(at: 0)
         XCTAssertEqual(sut.pickedMaterials.first, productMaterialList.materials.first)
@@ -146,5 +90,15 @@ class PickProductMaterailsViewModelTests: XCTestCase {
         sut.selectMaterial(at: 0)
         XCTAssertEqual(sut.pickedMaterials, [])
         XCTAssertFalse(sut.pickedMaterialIDs.contains(try XCTUnwrap(productMaterialList.materials.first?.id)))
+    }
+
+    private func makeMockProductMaterialList(file: StaticString = #filePath, line: UInt = #line) throws -> ProductMaterialList {
+        guard let data = self.cachedFileDataHelper.cachedFileData(name: "product_materials"),
+              let productMaterialList = try? JSONDecoder().decode(Response<ProductMaterialList>.self, from: data).data
+        else {
+            throw APIError.dataNotFoundError
+        }
+
+        return productMaterialList
     }
 }
