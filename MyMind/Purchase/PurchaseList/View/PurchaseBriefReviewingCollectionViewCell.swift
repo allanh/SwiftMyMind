6b24a78d5d4477e91d3985f67871025e1318baf4
Nelson Chan <nelson.chan@udnshopping.com>
2021-07-02 17:22:41 +0800

    feat: add review collection view cell

---
+++ MyMind/Purchase/PurchaseList/View/PurchaseBriefReviewingCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseBriefReviewingCollectionViewCell.swift
+//  MyMind
+//
+//  Created by Nelson Chan on 2021/7/2.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseBriefReviewingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
