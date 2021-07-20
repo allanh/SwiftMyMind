68682ef35411e453725d5611b89e8d58196eaa29
Nelson Chan <nelson.chan@udnshopping.com>
2021-07-20 12:17:42 +0800

    feat: main function

--- MyMind/MainFunctionEntry/MainFunctionEntryViewController.swift
+++ MyMind/MainFunctionEntry/MainFunctionEntryViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 enum MainFunctoinType: String {
     case purchaseApply = "採購申請"
+    case purchaseReview = "採購審核"
     case paybill = "付款單"
     case saleChart = "銷售報表"
     case revenueChart = "營收報表"
@@ -20,21 +21,23 @@ final class MainFunctionEntryViewController: NiblessViewController {
     
     typealias FunctionControlInfo = (type: MainFunctoinType, imageName: String)
     private var functionControls: [MainFunctionControl] = []
+//    private var functionControls: [UIView] = []
 
 
     private let functionControlInfos: [FunctionControlInfo] = [
         (.purchaseApply, "buy_icon"),
-        (.paybill, "pay_icon"),
-        (.saleChart, "sale_icon"),
-        (.revenueChart, "renvenu_icon"),
-        (.systemSetting, "system_setting_icon"),
+        (.purchaseReview, "examine_icon"),
+//        (.paybill, "pay_icon"),
+//        (.saleChart, "sale_icon"),
+//        (.revenueChart, "renvenu_icon"),
+//        (.systemSetting, "system_setting_icon"),
         (.accountSetting, "account_setting_icon")
     ]
 
     private let stackView: UIStackView = UIStackView {
         $0.spacing = 30
         $0.axis = .vertical
-        $0.distribution = .fillEqually
+        $0.distribution = .fillProportionally
     }
 
     override func viewDidLoad() {
@@ -57,46 +60,48 @@ final class MainFunctionEntryViewController: NiblessViewController {
         view.addSubview(stackView)
     }
 
+    private let rowHeight: CGFloat = 120
+    private let columns = 2
     private func activateConstraintsStackView() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        let top = stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20)
         let centerX = stackView.centerXAnchor
             .constraint(equalTo: view.centerXAnchor)
-        let centerY = stackView.centerYAnchor
-            .constraint(equalTo: view.centerYAnchor)
         let width = stackView.widthAnchor
             .constraint(equalTo: view.widthAnchor, multiplier: 0.8)
-        let height = stackView.heightAnchor
-            .constraint(equalTo: stackView.widthAnchor, multiplier: 1.7)
+        let numberOfRows = functionControlInfos.count / columns + 1
+        let height = stackView.heightAnchor.constraint(equalToConstant: CGFloat(numberOfRows)*rowHeight+stackView.spacing)
         NSLayoutConstraint.activate([
-            centerX, centerY, width, height
+            centerX, top, width, height
         ])
     }
-
     private func constructStackViews() {
-        var horizontalStackView: UIStackView? = creatHorizontalStackView()
+        var horizontalStackView: UIView? = creatHorizontalView()
         for index in 0..<functionControls.count {
             if horizontalStackView == nil {
-                horizontalStackView = creatHorizontalStackView()
+                horizontalStackView = creatHorizontalView()
             }
-            horizontalStackView?.addArrangedSubview(functionControls[index])
+            horizontalStackView?.addSubview(functionControls[index])
             if (index + 1) % 2 == 0{
                 self.stackView.addArrangedSubview(horizontalStackView!)
                 horizontalStackView = nil
             }
         }
+        if let lastView = horizontalStackView {
+            self.stackView.addArrangedSubview(lastView)
+        }
     }
-
-    private func creatHorizontalStackView() -> UIStackView {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.spacing = 40
-        return stackView
+    private func creatHorizontalView() -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.stackView.bounds.width, height: rowHeight))
+        return view
     }
 
     private func creatFuncitonControls() {
-        for item in functionControlInfos {
-            let functionControl = MainFunctionControl(mainFunctionType: item.type)
+        let width = view.bounds.width*0.8
+        let itemWidth = width * 0.45
+        for (index, item) in functionControlInfos.enumerated() {
+            let x = index%2 == 0 ? 0 : width-itemWidth
+            let functionControl = MainFunctionControl(frame: CGRect(x: x, y: 0, width: itemWidth, height: rowHeight),  mainFunctionType: item.type)
             functionControl.imageView.image = UIImage(named: item.imageName)
             functionControl.addTarget(self, action: #selector(didTapFunctionControl(_:)), for: .touchUpInside)
             functionControls.append(functionControl)
@@ -107,13 +112,13 @@ final class MainFunctionEntryViewController: NiblessViewController {
     private func didTapFunctionControl(_ sender: MainFunctionControl) {
         switch sender.functionType {
         case .purchaseApply:
-            let navigationController = UINavigationController(rootViewController: PurchaseListViewController(purchaseListLoader: MyMindPurchaseAPIService.shared))
-            navigationController.modalPresentationStyle = .fullScreen
-            show(navigationController, sender: nil)
+            show(PurchaseListViewController(purchaseListLoader: MyMindPurchaseAPIService.shared), sender: nil)
         case .paybill:
             let storyboard: UIStoryboard = UIStoryboard(name: "TOTP", bundle: nil)
             let viewController = storyboard.instantiateViewController(withIdentifier: "SecretListViewControllerNavi")
             present(viewController, animated: true, completion: nil)
+        case .purchaseReview:
+            show(PurchaseListViewController(purchaseListLoader: MyMindPurchaseReviewAPIService.shared), sender: nil)
         case .accountSetting:
             if let settingViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "Setting") as? SettingViewController {
                 settingViewController.delegate = self
