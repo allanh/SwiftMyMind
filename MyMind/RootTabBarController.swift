91a3feb411c1459634457785a4fb3ce29539edf7
Nelson Chan <nelson.chan@udnshopping.com>
2021-07-20 12:16:06 +0800

    fix: back title

--- MyMind/RootTabBarController.swift
+++ MyMind/RootTabBarController.swift
@@ -26,6 +26,7 @@ final class RootTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "MyMind"
+        navigationItem.backButtonTitle = ""
         generateHomeViewController()
         generateMainFunctionEntryViewController()
         viewControllers = contentViewControlelrs
