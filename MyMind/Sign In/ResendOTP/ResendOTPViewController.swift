84965b62489a13e7a7f0aec242920b576fd405c1
Nelson Chan <nelson.chan@udnshopping.com>
2021-07-20 10:18:52 +0800

    feat: add files

---
+++ MyMind/Sign In/ResendOTP/ResendOTPViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResendOTPViewController.swift
+//  MyMind
+//
+//  Created by Nelson Chan on 2021/7/19.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import Foundation
