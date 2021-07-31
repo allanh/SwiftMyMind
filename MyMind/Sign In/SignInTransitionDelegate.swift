5fc4a6622d963e70b0bc55204e1d2b18fcebb5f2
Nelson Chan <nelson.chan@udnshopping.com>
2021-07-31 13:03:00 +0800

    feat: sign in presentation

---
+++ MyMind/Sign In/SignInTransitionDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SignInTransitionDelegate.swift
+//  MyMind
+//
+//  Created by Nelson Chan on 2021/7/31.
+//  Copyright © 2021 United Digital Intelligence. All rights reserved.
+//
+
+import Foundation
