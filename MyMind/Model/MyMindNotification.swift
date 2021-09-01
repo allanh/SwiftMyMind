a2b7fb4bc84b92c20226e60fa2332b65dc3e9a44
Nelson Chan <nelson.chan@udnshopping.com>
2021-09-01 12:25:10 +0800

    feat: retrieve announcement

--- MyMind/Model/MyMindNotification.swift
+++ MyMind/Model/MyMindNotification.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+// MARK: -MyMindNotification
+struct MyMindNotification: Codable {
+    enum NotificationType: String, Codable {
+        case ANNOUNCEMENT
+    }
+    private enum CodingKeys: String, CodingKey {
+        case title, type, importance
+        case subTitle = "sub_title"
+        case top = "is_top"
+        case started = "started_at"
+        case readed = "read_at"
+        case id = "notification_id"
+    }
+    let title: String
+    let subTitle: String
+    let type: NotificationType
+    let importance: Importance
+    let top: Bool
+    let started: String
+    let readed: String?
+    let id: Int
+}
+// MARK: -MyMindNotificationList
+struct MyMindNotificationList: Codable {
+    let items: [MyMindNotification]
+    let unreaded: Int
+    private enum CodingKeys: String, CodingKey {
+        case items = "detail"
+        case unreaded = "unread_count"
+    }
+}
