3d7c5f66fc26dda4a24edb3ea298fffbb1f71d22
Nelson Chan <nelson.chan@udnshopping.com>
2021-09-01 14:23:47 +0800

    fix: value type

--- MyMind/Model/MyMindNotification.swift
+++ MyMind/Model/MyMindNotification.swift
@@ -25,9 +25,28 @@ struct MyMindNotification: Codable {
     let type: NotificationType
     let importance: Importance
     let top: Bool
-    let started: String
-    let readed: String?
+    let started: Date
+    let readed: Date?
     let id: Int
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        subTitle = try container.decode(String.self, forKey: .subTitle)
+        type = try container.decode(NotificationType.self, forKey: .type)
+        importance = try container.decode(Importance.self, forKey: .importance)
+        top = try container.decode(Bool.self, forKey: .top)
+        let string = try container.decode(String.self, forKey: .started)
+        let formatter = DateFormatter {
+            $0.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        }
+        started = formatter.date(from: string) ?? .distantPast
+        if let string = try? container.decode(String.self, forKey: .readed) {
+            readed = formatter.date(from: string)
+        } else {
+            readed = nil
+        }
+        id = try container.decode(Int.self, forKey: .id)
+    }
 }
 // MARK: -MyMindNotificationList
 struct MyMindNotificationList: Codable {
